07991f207cf3b931e0b15dbb3ccd4c02ac907c70
Emilio Garro Rangel <emonteror@ucenfotec.ac.cr>
2025-03-22 23:42:43 -0600

    added db config

---
+++ database.py
@@ -0,0 +1,10 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+SQLALCHEMY_DATABASE_URL = "sqlite://postgres:1A0b2C9d3E@localhost:5432/QuizApplication"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
