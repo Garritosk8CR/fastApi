7b68477dd22e5b45ced4ce10cc62780be642784b
Emilio Garro Rangel <emonteror@ucenfotec.ac.cr>
2025-03-23 14:45:54 -0600

    con to the db and setup dependy injection with db

--- models.py
+++ models.py
@@ -4,11 +4,13 @@ from database import Base
 
 class Question(Base):
     __tablename__ = "questions"
+    
     id = Column(Integer, primary_key=True, index=True)
     question_text = Column(String, index=True)
     
 class Choices(Base):
     __tablename__ = "choices"
+
     id = Column(Integer, primary_key=True, index=True)
     choice_text = Column(String, index=True)
     is_correct = Column(Boolean, default=False)
