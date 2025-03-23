0fa32f1c2200de511eca03ca099e5bd1d28d2ac5
Emilio Garro Rangel <emonteror@ucenfotec.ac.cr>
2025-03-23 15:46:18 -0600

    Added FastApi and first endpoint questions

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite://postgres:1A0b2C9d3E@localhost:5432/QuizApplication"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:1A0b2C9d3E@localhost:5432/QuizApplication"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
--- main.py
+++ main.py
@@ -1,8 +1,13 @@
-from fastApi import FastAPI, HTTPEexception, Depends
+from fastapi import FastAPI, HTTPException, Depends
 from pydantic import BaseModel
 from typing import List, Annotated
+import models
+from database import engine, SessionLocal
+from sqlalchemy.orm import Session
 
 app = FastAPI()
+
+models.Base.metadata.create_all(bind=engine)
 
 class ChoiceBase(BaseModel):
     choice_text: str
@@ -10,4 +15,24 @@ class ChoiceBase(BaseModel):
 
 class QuestionBase(BaseModel):
     question_text: str
-    choices: List[ChoiceBAse]
+    choices: List[ChoiceBase]
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+@app.post("/questions/", response_model=QuestionBase)
+async def create_questions(question: QuestionBase, db: db_dependency):
+    db_question = models.Question(question_text=question.question_text)
+    db.add(db_question)
+    db.commit()
+    db.refresh(db_question)
+    for choice in question.choices:
+        db_choice = models.Choices(choice_text=choice.choice_text, is_correct=choice.is_correct, question_id=db_question.id)
+        db.add(db_choice)
+    db.commit()
