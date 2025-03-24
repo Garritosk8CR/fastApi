db588d8ed807d2374e73476ce04c9305c4fd1bb1
Emilio Garro Rangel <emonteror@ucenfotec.ac.cr>
2025-03-23 20:56:46 -0600

    get endpoint

--- main.py
+++ main.py
@@ -26,7 +26,14 @@ def get_db():
 
 db_dependency = Annotated[Session, Depends(get_db)]
 
-@app.post("/questions/", response_model=QuestionBase)
+@app.get("/questions/{question_id}")
+async def read_question(question_id: int, db: db_dependency):
+    result = db.query(models.Question).filter(models.Question.id == question_id).first()
+    if not result:
+        raise HTTPException(status_code=404, detail="Question not found")
+    return result
+
+@app.post("/questions/")
 async def create_questions(question: QuestionBase, db: db_dependency):
     db_question = models.Question(question_text=question.question_text)
     db.add(db_question)
@@ -36,3 +43,4 @@ async def create_questions(question: QuestionBase, db: db_dependency):
         db_choice = models.Choices(choice_text=choice.choice_text, is_correct=choice.is_correct, question_id=db_question.id)
         db.add(db_choice)
     db.commit()
+
--- test_questions.py
+++ test_questions.py
@@ -25,15 +25,12 @@ def test_create_questions(test_db):
     }
     response = client.post("/questions/", json=question_data)
     assert response.status_code == 200
-    response_data = response.json()
-    assert response_data is not None
 
 def test_create_random_question(test_db):
     # Use the test database in the test
     app.dependency_overrides[get_db] = lambda: test_db
     client = TestClient(app)
 
-        # Generate a random question with multiple random choices, 1 correct and the rest incorrect
     num_choices = 4
     question_data = {
         "question_text": "Random question?",
@@ -45,6 +42,28 @@ def test_create_random_question(test_db):
     response = client.post("/questions/", json=question_data)
     response.raise_for_status()
     assert response.status_code == 200
-    assert response.json()["question_text"] == question_data["question_text"]
-    assert len(response.json()["choices"]) == num_choices
-    assert sum(choice["is_correct"] for choice in response.json()["choices"]) == 1
+
+def test_read_question_not_found(test_db):
+    # Use the test database in the test
+    app.dependency_overrides[get_db] = lambda: test_db
+    client = TestClient(app)
+
+    # Create a question in the database
+    db_question = Question(question_text="Test question")
+    test_db.add(db_question)
+    test_db.commit()
+
+    response = client.get(f"/questions/{db_question.id + 1}")
+    assert response.status_code == 404
+    assert response.json()["detail"] == "Question not found"
+
+def test_read_question_found(test_db):
+    app.dependency_overrides[get_db] = lambda: test_db
+    client = TestClient(app)
+
+    db_question = Question(question_text="Test question")
+    test_db.add(db_question)
+    test_db.commit()
+
+    response = client.get(f"/questions/{db_question.id}")
+    assert response.status_code == 200
